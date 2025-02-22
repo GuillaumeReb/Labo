0b122e58944d204e05ad738770c6c0da2757bffe
Guillaume <guillaume.rebourgeon@hotmail.fr>
2025-02-22 15:03:07 +0100

    Mise en place de la nav

--- React_V1/cours-react-1/src/App.js
+++ React_V1/cours-react-1/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Path"*" fonctionne si jamais l'url ne correspond à rien */}
+        <Route path="*" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
